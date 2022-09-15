334961fbb0620f7475dec5f5152fc0031231fec7
GreatHayat <greathayat@gmail.com>
2022-09-15 13:47:02 +0500

    added error middleware

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const errorHandler = require("./middlewares/error");
+const routes = require("./routes");
 
 const app = express();
 
+// DB Connection
+mongoose
+  .connect("mongodb://localhost:27017/auth-db")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.log(`Couldn't connected to MongoDB`, error));
+
 // middlewares
 app.use(express.json());
+
+// API Endpoints
+app.use("/api", routes);
+
+// Custom Middlewares
+app.use(errorHandler);
 
 // PORT CONFIGURATION
 const port = process.env.PORT || 5001;
